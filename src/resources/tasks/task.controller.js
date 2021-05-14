0a10f63d23db7ca4d79325e8620594ca665f666d
Andrew <andslobby@gmail.com>
2021-05-14 11:49:58 +0300

    feat: add task.controller middleware

---
+++ src/resources/tasks/task.controller.js
@@ -0,0 +1,55 @@
+const tasksService = require('./task.service');
+
+const getAll = async (req, res) => {
+  const {boardId} = req.params;
+  const tasks = await tasksService.getAll(boardId);
+  res.status(200).json(tasks);
+};
+
+const create = async (req, res) => {
+  const {boardId} = req.params;
+  const {title, order, description, userId, columnId} = req.body;
+  const task = await tasksService.create({title, order, description, userId, boardId, columnId});
+  if (task) {
+    res.status(201).json(task);
+  }
+  else {
+  res.status(400).send('Couldn`t create task.');
+  }
+};
+
+const getById = async (req, res) => {
+  const {boardId, taskId:id} = req.params;
+  const task = await tasksService.getById({boardId, id});
+  if (task) {
+    res.status(200).json(task);
+  }
+  else {
+  res.status(404).send('Not found.');
+  }
+};
+
+const update = async (req, res) => {
+  const id = req.params.taskId;
+  const { title=undefined, order=undefined, description=undefined, userId=null, boardId=null, columnId=null } = req.body;
+  const task = await tasksService.update({id, title, order, description, userId, boardId, columnId});
+  if (task) {
+    res.status(200).json(task);
+  }
+  else {
+  res.status(404).send('Bad request.');
+  }
+};
+
+const deletById = async (req, res) => {
+  const {boardId, taskId:id} = req.params;
+  const task = await tasksService.deletById({boardId, id});
+  if (task) {
+    res.status(204).json(task);
+  }
+  else {
+  res.status(404).send('Not found.');
+  }
+};
+
+module.exports = {getAll, create, getById, update, deletById};
